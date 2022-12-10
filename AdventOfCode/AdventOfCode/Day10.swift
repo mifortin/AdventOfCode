26957e029c51b336ad298fabc0e7591bff5f63be
Michael <quirky-clearer-0c@icloud.com>
2022-12-10 10:32:15 -0500

    Make Day 10 pt 1 functional.

--- AdventOfCode/AdventOfCode/Day10.swift
+++ AdventOfCode/AdventOfCode/Day10.swift
@@ -7,6 +7,22 @@
 
 import Foundation
 
+
+func D10ComputeCycles (Current : Int, Parsed : ArraySlice<(Int, (Int) -> Int)>) -> ArraySlice<Int>
+{
+	if (Parsed.count == 0)
+	{
+		return ArraySlice<Int>()
+	}
+	else
+	{
+		let Start = Parsed.startIndex
+		let Value = Parsed[Start]
+		let Next = Value.1(Current)
+		return Array.init(repeating: Current, count: Value.0) + D10ComputeCycles(Current: Next, Parsed: Parsed[(Start+1)...])
+	}
+};
+
 func Day10() throws
 {
 	let Lines = try ReadFile("Day10-1").filter({ $0 != "" })
@@ -31,34 +47,16 @@ func Day10() throws
 		return (1000,{ (_ v:Int)->Int in return -10000 })
 	}
 	
-	var Value = 1
-	var Cycles = Array<Int>()
-	Cycles.append(0)	// Cycle 0, ignore.
-	for inst in parsed
-	{
-		for _ in 1...inst.0
-		{
-			Cycles.append(Value)
-		}
-		
-		Value = inst.1(Value);
-	}
+	let Cycles = [1] + D10ComputeCycles(Current: 1, Parsed: ArraySlice(parsed))
 	
 	let KeyCycles = [20,60,100,140,180,220]
 	
-	var Sums = 0
-	
-	for K in KeyCycles
-	{
-		let I = min(K, Cycles.count-1)
-		let Strength = Cycles[I] * K
+	let Sums = KeyCycles.reduce(1, {(Previous:Int, KeyIndex:Int) -> Int in
+		let I = min(KeyIndex, Cycles.count-1)
+		let Strength = Cycles[I] * KeyIndex
 		print("Cycle \(Cycles[I]) Strength: \(Strength)")
-		Sums += Strength
-	}
-	
-	//print(Lines)
-	//print(parsed)
-	//print(Cycles)
+		return Previous + Strength
+	})
 	
 	var Display = Array<String>()
 	var J = 1

19d60faed4c5999359b466ac69ba4c33655a8e3c
Michael <quirky-clearer-0c@icloud.com>
2022-12-17 17:01:19 -0500

    Binary version!  Yay!!!

--- AdventOfCode/AdventOfCode/Day17.swift
+++ AdventOfCode/AdventOfCode/Day17.swift
@@ -10,54 +10,55 @@ import DequeModule
 
 func Day17(_ File:String, Iterations: Int) throws
 {
-	let Lines = try ReadFile(File).joined()
+	let Lines = try ReadFile(File).joined().map({
+		if $0 == "<" { return -1 }
+		if $0 == ">" { return 1 }
+		
+		print("ERROR \($0)")
+		return 0
+	})
 	
 	//print(Lines.count)
 	
 	var LineIndex = 0
-	let Read = { ()->Character in
+	let Read = { ()->Int in
 		let CurIndex = LineIndex
 		LineIndex = (LineIndex + 1) % Lines.count
-		//print(Lines[Lines.index(Lines.startIndex, offsetBy: CurIndex)], terminator: "")
-		return Lines[Lines.index(Lines.startIndex, offsetBy: CurIndex)]
-	}
-	
-	let ReadOffset = {
-		() -> Int in
-		let L = Read()
-		return L == "<" ? -1 : (L == ">" ? 1 : 0)
+		return Lines[CurIndex]
 	}
 	
 	let Rocks = [
 		[
-			"===="
+			0b00111100
 		],
 		[
-			".+.",
-			"+++",
-			".+."
+			0b00010000,
+			0b00111000,
+			0b00010000
 		],
 		[
-			"..*",
-			"..*",
-			"***"
+			0b00001000,
+			0b00001000,
+			0b00111000
 		],
 		[
-			"|",
-			"|",
-			"|",
-			"|"
+			0b00100000,
+			0b00100000,
+			0b00100000,
+			0b00100000
 		],
 		[
-			"XX",
-			"XX"
+			0b00110000,
+			0b00110000
 		]
 	]
 	
-	let Empty = Array(repeating: Character("."), count: 7)
-	let Base = Array(repeating: Character("-"), count: 7)
 	
-	var PlayField = Deque<Array<Character> >()
+	
+	let Empty = 0b00000000_00000000
+	let Base = 0b11111111_11111111
+	
+	var PlayField = Deque<Int>()
 	PlayField.append(Base)
 	let AddEmptyLines = {
 		(NumLines:Int) in
@@ -70,7 +71,18 @@ func Day17(_ File:String, Iterations: Int) throws
 	let ShowPlayField = {
 		for X in PlayField
 		{
-			print("|\(String(X))|")
+			for Y in 1...7
+			{
+				if X & (1 << (8-Y)) != 0
+				{
+					print("#", terminator: "")
+				}
+				else
+				{
+					print(".", terminator: "")
+				}
+			}
+			print()
 		}
 		print()
 	}
@@ -80,13 +92,12 @@ func Day17(_ File:String, Iterations: Int) throws
 	
 	for I in 0..<Iterations
 	{
-		if I % Checkpoint == 0
+		if Checkpoint > 0 && I % Checkpoint == 0
 		{
 			print("=", terminator: "")
 		}
 		
-		let CurRock = Rocks[I % Rocks.count]
-		var CurX = 2
+		var CurRock = Rocks[I % Rocks.count]
 		var CurY = 0
 		
 		AddEmptyLines(3)
@@ -98,61 +109,59 @@ func Day17(_ File:String, Iterations: Int) throws
 			var OY = CurY
 			for L in CurRock
 			{
-				var OX = CurX
-				for C in L
+				if (DoShow)
 				{
-					if C != "."
-					{
-						if ((DoShow && PlayField[OY][OX] != ".")
-							|| (!DoShow && PlayField[OY][OX] != C))
-						{
-							print("ERROR!")
-						}
-						PlayField[OY][OX] = DoShow ? C : "."
-					}
-					OX += 1
+					PlayField[OY] = PlayField[OY] | L
+				}
+				else
+				{
+					PlayField[OY] = PlayField[OY] & ~L
 				}
 				OY += 1
 			}
 		}
-		
-		let ShowState = {
-			WriteRock(true)
-			//ShowPlayField()
-			WriteRock(false)
-		}
-		//ShowState()
 		
 		while (true)
 		{
 			let TryPlace = {
 				(Dx:Int, Dy:Int) -> Bool in
 				
-				let NX = min(max(CurX + Dx, 0),7 - CurRock[0].count)
-				if NX != CurX + Dx
+				// We could have a stencil / shadow that moves to avoid the loops.
+				var RC = CurRock
+				if (Dx > 0)
 				{
-					return false
+					RC = CurRock.map( { $0 >> 1 } )
+					
+					for R in RC
+					{
+						if 0 != (R & 0x1) { return false }
+					}
 				}
+				if (Dx < 0)
+				{
+					RC = CurRock.map( { $0 << 1 })
+					
+					for R in RC
+					{
+						let Compare = 1 << 8
+						let Remaining = Compare & R
+						if 0 != (Remaining) {return false}
+					}
+				}
+				
 				let NY = CurY + Dy
 				
 				var OY = NY
-				for L in CurRock
+				for L in RC
 				{
-					var OX = NX
-					for C in L
-					{
-						if C != "." && PlayField[OY][OX] != "."
-						{
-							return false
-						}
-						OX += 1
-					}
+					if L & PlayField[OY] != 0 { return false }
+					
 					OY += 1
 				}
 				return true
 			}
 			
-			let Dir = ReadOffset()
+			let Dir = Read()
 			if (Dir == 0)
 			{
 				print("ERROR")
@@ -162,17 +171,23 @@ func Day17(_ File:String, Iterations: Int) throws
 				print("ERROR!")
 			}
 			
-			//print(Dir)
 			if (TryPlace(Dir, 0))
 			{
-				CurX += Dir
-				ShowState()
+				if Dir > 0	{ CurRock = CurRock.map({$0 >> 1}) }
+				if Dir < 0  { CurRock = CurRock.map({ $0 << 1})}
+				
+				//WriteRock(true)
+				//ShowPlayField()
+				//WriteRock(false)
 			}
 			
 			if (TryPlace(0, 1))
 			{
 				CurY += 1
-				ShowState()
+				
+				//WriteRock(true)
+				//ShowPlayField()
+				//WriteRock(false)
 			}
 			else
 			{
@@ -182,7 +197,7 @@ func Day17(_ File:String, Iterations: Int) throws
 		
 		WriteRock(true)
 		
-		while PlayField[0].reduce(0, { $0 + ($1 == "." ? 1 : 0)}) == 7
+		while PlayField[0] == 0
 		{
 			_ = PlayField.popFirst()
 		}
@@ -198,11 +213,9 @@ func Day17() throws
 {
 	
 	try Day17("Day17-Sample", Iterations: 2022)
-	//try Day17("Day17-Test")
 	try Day17("Day17-1", Iterations: 2022) // 3159 was perfect...
 	
 	
 	try Day17("Day17-Sample", Iterations: 1000000000000)
-	//try Day17("Day17-Test")
 	try Day17("Day17-1", Iterations: 1000000000000)
 }
